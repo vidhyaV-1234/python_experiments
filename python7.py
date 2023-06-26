741b10e1dfc9da40956cc3b82e9a3355d21ce864
vidhyaV-1234 <133896762+vidhyaV-1234@users.noreply.github.com>
2023-06-26 20:23:17 +0530

    set

--- python7.py
+++ python7.py
@@ -1,7 +1,10 @@
+#to check one set is subset of another set
 x=set(input().split())
 y=set(input().split())
+#to check x is a subset of y
 if x.issubset(y):
     print("x is a subset of y")
+#to check y is a subset of x    
 elif y.issubset(x):
     print("y is a subset of x")
 else:
