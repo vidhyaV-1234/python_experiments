2ce2bed1927ad375d0454ac707853f2b1a1550af
vidhyaV-1234 <133896762+vidhyaV-1234@users.noreply.github.com>
2023-06-26 19:31:37 +0530

    list_ascending_descending_order

--- exp_3/python3.py
+++ exp_3/python3.py
@@ -1,5 +1,7 @@
-a=list(map(int,input().split()))
+a=list(map(int,input("enter the value for list").split()))
+#checking if it is in descending order
 if a[0]>a[1]:
     print("Descending order")
+#else printing ascending order    
 else:
     print("Ascending order")
