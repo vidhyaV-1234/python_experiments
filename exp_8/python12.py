92bc1dcd01a8b482ee6613337195e15e67dafa79
vidhyaV-1234 <133896762+vidhyaV-1234@users.noreply.github.com>
2023-06-26 20:38:50 +0530

    Add files via upload

---
+++ exp_8/python12.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+animals=pd.Series({'elephant':200,'horse':300,'rabbit':2050})
+incre_pop=pd.Series({'elephant':100,'horse':50,'rabbit':200})
+total_pop=animals+incre_pop
+print(total_pop)
+total_pop=animals-incre_pop
+print(total_pop)
+total_pop=animals*incre_pop
+print(total_pop)
+total_pop=animals/incre_pop
+print(total_pop)
