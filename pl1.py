9a7fba6bc76f597ac4a7102f03bb8ea9c16a7bb0
vidhyaV-1234 <133896762+vidhyaV-1234@users.noreply.github.com>
2023-06-26 19:14:08 +0530

    Update pl1.py

--- pl1.py
+++ pl1.py
@@ -1,5 +1,5 @@
 #list-index (positive and negative)
-a=list(map(int,input().split()))
+a=list(map(int,input("enter elements for list").split()))
 n=int(input("enter the element wants to be searched"))
 d=a.index(n)
 print(d)
