a3983f1c3610b3104e9edf74e3d623536e0c80e9
vidhyaV-1234 <133896762+vidhyaV-1234@users.noreply.github.com>
2023-06-26 18:54:23 +0530

    odd_or_even.py

--- python1.py
+++ python1.py
@@ -1,10 +1,13 @@
 x=int(input("enter the integer" ))
 fact=1
+#checking it is odd or even
 if x%2==1:
+    #if it is odd doing factorial
     for i in range(1,x+1):
         fact=fact*i
     print(fact)
 else:
+    #else checking it is palindrom or not
     c=str(x)
     if c[::-1]== str(x):
         print("it is palindrom")
