eebe8428ec5a1fcb2da8ab6b8c95de09f676d63d
vidhyaV-1234 <133896762+vidhyaV-1234@users.noreply.github.com>
2023-06-26 18:57:17 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,20 @@
+def print_string(str1,str2):
+    i=len(str1)
+    j=len(str2)
+    c=[]
+    while i>0 and j>0:
+        if i==j:
+            c.append(i) 
+            j-=1
+        i-=1
+    if len(str2)==len(c):
+         return 1
+    else:
+         return 0
+str1=input("enter the string")
+str2=input("enter the string2")
+if(print_string(str1,str2)):
+    print("yes")
+else:
+    print("no")
+        
