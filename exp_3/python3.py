8dcf415c67d18ce2ebde3dc64b394358c19cf67f
vidhyaV-1234 <133896762+vidhyaV-1234@users.noreply.github.com>
2023-07-27 22:21:27 +0530

    ascending_and_descending

--- exp_3/python3.py
+++ exp_3/python3.py
@@ -1,7 +1,17 @@
 a=list(map(int,input("enter the value for list").split()))
 #checking if it is in descending order
-if a[0]>a[1]:
-    print("Descending order")
-#else printing ascending order    
+count=1
+count2=1
+for i in range(len(a)):
+    if (i+1)!=len(a):
+        if a[i]>a[i+1]:
+            count+=1
+        if a[i]<a[i+1]: 
+            count2+=1
+if len(a)==count:
+    print("it is in descending order")
+elif len(a)==count2:
+    print("it is in ascending order")
 else:
-    print("Ascending order")
+    print("it is mixed list")
+    
